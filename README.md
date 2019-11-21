36792e2ec403fceeb6b9e17e5c79a16da2913f07
Yuyuan Zhang <45277390+devlandunion@users.noreply.github.com>
2019-11-21 15:27:22 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# promenad-cropper
-Created with CodeSandbox
+# Vue-avatar-cropper demo
+
+A Vue.js demo project for [Vue-avatar-cropper](https://github.com/overtrue/vue-avatar-cropper).
+
+[![Edit test-project](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/overtrue/vue-avatar-cropper-demo/tree/master/)
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <form method="post">
+      <avatar/>
+    </form>
+  </div>
+</template>
+
+<script>
+import Avatar from './components/Avatar'
+
+export default {
+  name: 'App',
+  components: {
+    Avatar
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
